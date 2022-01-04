6b020196c7c0d5e6fbe8d447fa4d07d0200998e4
Zeynep <zeynepkaya_26@hotmail.com>
2022-01-04 02:30:57 +0000

    BootcampX Queries 3 - Group By

--- 2_queries_joins/3.sql
+++
@@ -1,6 +0,0 @@
-SELECT students.name as student, count(assignment_submissions.*) as total_submissions
-FROM assignment_submissions
-JOIN students ON students.id = student_id
-WHERE students.end_date IS NULL
-GROUP BY students.name
-HAVING count(assignment_submissions.*) < 100;
