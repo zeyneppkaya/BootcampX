1cb35004b37dc14b1b745aab8d2ae793c7977141
Zeynep <zeynepkaya_26@hotmail.com>
2022-01-07 01:40:19 +0000

    Adding parameterized Query to my sql

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort 
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher ;
-`)
+`;
+
+pool.query(queryString , values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort} : ${user.teacher} `);
